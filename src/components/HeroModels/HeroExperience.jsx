80f4133896395b911415740e0560fac755d50300
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-10-27 19:16:32 +0530

    add react responsive library

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -4,11 +4,19 @@ import React from 'react'
 import { MeshStandardMaterial } from 'three'
 
 const HeroExperience = () => {
+    const isTablet=false;
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45}}>
        <ambientLight intensity={0.2} color="#1a1a40" /> 
        <directionalLight position={[5, 5, 5]} intensity={5}/>
        <OrbitControls
+       enablePan={false}
+       enableZoom={!isTablet}
+       maxDistance={20}
+       minDistance={5}
+       minPolarAngle={Math.PI / 5}
+       maxPolarAngle={Math.PI / 2}
+
        />
 
        <mesh>
