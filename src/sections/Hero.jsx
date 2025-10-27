5f091ef585a2d4d774aa0a1bbf2b07a9db8ccdd3
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-10-27 17:29:30 +0530

    create a folder named HeroNamed & a file named HeroExperience.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,6 +13,7 @@ const Hero = () => {
         </div>
         <div className='hero-layout'>
             <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5 '>
+
                 <div className='flex flex-col gap-7'>
                     <div className='hero-text'>
                         <h1>Shaping
@@ -45,6 +46,8 @@ const Hero = () => {
                     />
                 </div>
             </header>
+            
+
         </div>
     </section>
   )
